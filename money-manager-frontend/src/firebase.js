57c96ce647b459a4c39c44c1fe5b2f4c64afc6e1
Bhuvanesh N <105157420+Bhuvaneshmaker@users.noreply.github.com>
2026-02-06 00:03:29 +0530

    Update firebase.js

--- money-manager-frontend/src/firebase.js
+++ money-manager-frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdJCCCLpFqnGL9Ls5yBA4I2E7kNXXWSNs',
-  authDomain: 'rental-site-b80a8.firebaseapp.com',
-  projectId: 'rental-site-b80a8',
-  storageBucket: 'rental-site-b80a8.firebasestorage.app',
-  messagingSenderId: '164233970553',
-  appId: '1:164233970553:web:ec30666105d5b855ca25a8',
-  measurementId: 'G-G355KB9H8G',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
